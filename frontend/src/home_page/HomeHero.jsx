52c861b04ce119546e3a54dfbfa42629dffefbf4
Danny-jin <dannyjin0215@gmail.com>
2025-02-20 21:13:05 -0800

    Homepage section update - part of the home hero page, css & jsx file

---
+++ frontend/src/home_page/HomeHero.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './HomeHero.css';
+import heroImage from './homepage-plant.png';
+
+function HomeHero() {
+  return (
+    <section className="home-hero">
+      <div className="hero-content">
+        <h1>WELCOME</h1>
+        <p>
+        Welcome to Urban Gardening for Beginners at UCSD! 
+        Get ready to dig in and start your urban gardening journey today!
+        </p>
+        <button className="hero-button">LEARN MORE</button>
+      </div>
+      
+      <div className="hero-image">
+        <img src={heroImage} alt="A person holding a potted plant" />
+      </div>
+    </section>
+  );
+}
+
+export default HomeHero;
