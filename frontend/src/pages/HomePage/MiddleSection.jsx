43c1b9e5d263b92992aab8813591b67324af60f2
Kathy <kathycharry27@gmail.com>
2025-03-01 23:29:15 -0800

    Created middle section of the home page

---
+++ frontend/src/pages/HomePage/MiddleSection.jsx
@@ -0,0 +1,13 @@
+import './MiddleSection.css'
+
+function MiddleSection() {
+    return (
+        <>
+        <div className="middle-section">
+            <h3>“The love of gardening is a seed once sown that never dies”</h3>
+        </div>
+        </>
+    )
+}
+
+export default MiddleSection;
--- frontend/src/pages/HomePage/homepage.jsx
+++ frontend/src/pages/HomePage/homepage.jsx
@@ -1,10 +1,12 @@
 import HomeHero from './HomeHero.jsx'
 import ContactUs from './contactus.jsx'
+import MiddleSection from './MiddleSection.jsx';
 
 function HomePage() {
     return (
         <>
             <HomeHero/>
+            <MiddleSection/>
             <ContactUs/>
         </>
     )
