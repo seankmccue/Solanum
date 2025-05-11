ad1b3b7ee1c58f3e4c71d6265171e833754fe6fc
cecilia q <ceciliaq@cecilias-MacBook-Air.local>
2025-05-11 13:37:47 -0700

    added top of locate garden portion

--- frontend/src/pages/LocatePage/LocateTop.jsx
+++ frontend/src/pages/LocatePage/LocateTop.jsx
@@ -1,5 +1,8 @@
-
 import "./LocateTop.css";
+import { FaLeaf } from "react-icons/fa6";
+import mapImage from "../../assets/gardenMap.png";
+
+
 
 function Top() {
   return (
@@ -10,9 +13,21 @@ function Top() {
           Lorem ipsum dolor sit amet. Sed nemo explicabo est earum nostrum quo
           voluptates omnis eos culpa magnam. Ut vero dolor et eveniet impedit
           est recusandae debitis. Est quibusdam delectus eum nihil minima non
+          explicabo placeat. Lorem ipsum dolor sit amet. Sed nemo explicabo est earum nostrum quo
+          voluptates omnis eos culpa magnam. Ut vero dolor et eveniet impedit
+          est recusandae debitis. Est quibusdam delectus eum nihil minima non
+          explicabo placeat. Lorem ipsum dolor sit amet. Sed nemo explicabo est earum nostrum quo
+          voluptates omnis eos culpa magnam. Ut vero dolor et eveniet impedit
+          est recusandae debitis. Est quibusdam delectus eum nihil minima non
           explicabo placeat.
         </p>
       </div>
+
+      <div className="leaf-banner">
+        <FaLeaf className="leaf-icon" />
+        <p className="icon-text">The leaf icon signifies the nearby garden location</p>
+      </div>
+      <img src={mapImage} alt="UCSD Garden Map" className="garden-map" />
     </div>
   );
 }
